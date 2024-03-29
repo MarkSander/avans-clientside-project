b1bdfc90cfe5103e4e8da90d232c5555dadc6916
MarkSander <markwillem.sander@gmail.com>
2024-03-29 22:01:45 +0100

    added user backend

---
+++ libs/shared/api/src/lib/models/user.interface.ts
@@ -0,0 +1,21 @@
+export type UserId = string;
+
+export interface IUser {
+  _id: UserId;
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+export enum UserRole {
+  Admin = 'Admin',
+  Visitor = 'Visitor',
+}
+
+export type ICreateUser = Pick<
+  IUser,
+  'username' | 'email' | 'password' | 'createdAt'
+>;
+export type IUpdateUser = Partial<Omit<IUser, 'id'>>;
+export type IUpsertUser = IUser;
